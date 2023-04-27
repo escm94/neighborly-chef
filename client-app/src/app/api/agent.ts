da02d310afe3383843e564c50153fe0e85b8f41c
Matt Escalante <m.escalante94@gmail.com>
2023-04-27 15:10:02 -0400

    add type safety for axios requests

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,18 +1,20 @@
 import axios, { AxiosResponse } from 'axios';
+import { Meal } from '../models/meal';
 
 axios.defaults.baseURL = 'http://localhost:5000/api';
 
-const responseBody = (response: AxiosResponse) => response.data;
+const responseBody = <T>(response: AxiosResponse<T>) => response.data;
 
 const requests = {
-  get: (url: string) => axios.get(url).then(responseBody),
-  post: (url: string, body: {}) => axios.post(url, body).then(responseBody),
-  put: (url: string, body: {}) => axios.put(url, body).then(responseBody),
-  del: (url: string) => axios.delete(url).then(responseBody),
+  get: <T>(url: string) => axios.get<T>(url).then(responseBody),
+  post: <T>(url: string, body: {}) =>
+    axios.post<T>(url, body).then(responseBody),
+  put: <T>(url: string, body: {}) => axios.put<T>(url, body).then(responseBody),
+  del: <T>(url: string) => axios.delete<T>(url).then(responseBody),
 };
 
 const Meals = {
-  list: () => requests.get('/meals'),
+  list: () => requests.get<Meal[]>('/meals'),
 };
 
 const agent = {
